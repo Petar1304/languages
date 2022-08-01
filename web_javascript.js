9a47de4e3ff36fb6b61f44f7e0bcd72122782648
Petar <67274324+Petar1304@users.noreply.github.com>
2022-08-01 10:22:12 +0200

    Create web_javascript.js

---
+++ web_javascript.js
@@ -0,0 +1,22 @@
+document.querySelector(cssSelector)
+document.querySelectorAll(cssSelector)
+
+document.getElementById(id)
+document.getElementsByTagName(name)
+document.getElementByClassName(name)
+
+document.createElement()
+
+elem = document.getElementById()
+elem.addEventListener('click', function)
+/*
+  click
+  mouseenter
+  mouseover
+  mouseout
+  keydown
+  change
+*/
+
+setInterval(fn, 1000); // execute fn every 1000 miliseconds
+clearInterval(fn);
